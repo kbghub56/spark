f26e40953959850773dab37331d55d2ffc5bbfd4
Kabir Borle <kabirborle@gmail.com>
2024-02-09 16:50:37 -0600

    Initial Commit

---
+++ spark/sparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkApp.swift
+//  spark
+//
+//  Created by Kabir Borle on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct sparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

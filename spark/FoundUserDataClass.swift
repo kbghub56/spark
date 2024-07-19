e9d213066e2081400938eff1184a8f3fe1f5228c
kbghub56 <kabirborle@gmail.com>
2024-07-19 01:30:06 -0500

    fix for add friends "user name" glitch

---
+++ spark/FoundUserDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  FoundUserDataClass.swift
+//  spark
+//
+//  Created by Kabir Borle on 7/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+class FoundUserData: ObservableObject {
+    @Published var user: User?
+}
